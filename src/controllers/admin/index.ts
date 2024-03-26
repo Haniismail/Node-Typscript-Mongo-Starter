5f8a7ad09d8d09a730c74687db488385254cea03
hani.ismail <haniismail@takiacademyteam.com>
2024-03-26 09:41:06 +0100

    final commit

--- src/controllers/admin/index.ts
+++ src/controllers/admin/index.ts
@@ -1,11 +1,10 @@
-import { createAdmin } from "./createAdmin.controller";
-import { deleteAdmin } from "./deleteAdmin.controller";
-import { getAdmin } from "./getAdmin.controller";
-import { updateAdmin } from "./updateAdmin.controller";
-import { getAllAdmins } from "./getAllAdmins.controller";
-import countaAllAdmins from "./countAll.controller";
-import { getAllNonAdmins } from "./getAllNonAdmins.controller";
-import { getMyProfile } from "../admin/auth/getMe";
+import { createAdmin } from './createAdmin.controller';
+import { deleteAdmin } from './deleteAdmin.controller';
+import { getAdmin } from './getAdmin.controller';
+import { updateAdmin } from './updateAdmin.controller';
+import { getAllAdmins } from './getAllAdmins.controller';
+import { getAllNonAdmins } from './getAllNonAdmins.controller';
+import { getMyProfile } from '../admin/auth/getMe';
 
 export default {
   createAdmin,
@@ -13,7 +12,6 @@ export default {
   getAdmin,
   updateAdmin,
   getAllAdmins,
-  countaAllAdmins,
   getAllNonAdmins,
   getMyProfile,
 };
--- src/routes/v1/admin/admin.ts
+++ src/routes/v1/admin/admin.ts
@@ -1,52 +1,51 @@
-import express from "express";
-import role from "../../../helpers/role";
-import authentication from "../../../auth/authentication";
-import authorization from "../../../auth/authorization";
-import { RoleCode } from "../../../database/model/Role";
-import adminController from "../../../controllers/admin";
-import validator, { ValidationSource } from "../../../helpers/validator";
-import schema from "./schema";
-import uploadMediaFilesToThisFolder from "../../../helpers/fileUpload/uploadDestiny";
-import FileUploadHandler from "../../../helpers/fileUpload/index";
-import { mediaFodlerNameSchemaAndObjectId } from "../global.routes.schema";
+import express from 'express';
+import role from '../../../helpers/role';
+import authentication from '../../../auth/authentication';
+import authorization from '../../../auth/authorization';
+import { RoleCode } from '../../../database/model/Role';
+import adminController from '../../../controllers/admin';
+import validator, { ValidationSource } from '../../../helpers/validator';
+import schema from './schema';
+import uploadMediaFilesToThisFolder from '../../../helpers/fileUpload/uploadDestiny';
+import FileUploadHandler from '../../../helpers/fileUpload/index';
+import { mediaFodlerNameSchemaAndObjectId } from '../global.routes.schema';
 
 const router = express.Router();
 const fileUploadHandler = new FileUploadHandler();
 
 router.use(
-  "/",
+  '/',
   authentication,
   authorization([RoleCode.ADMIN, RoleCode.SUPERADMIN])
 );
 
-router.get("/all", adminController.getAllAdmins);
-router.get("/all/non-admins", adminController.getAllNonAdmins);
-router.get("/all/count", adminController.countaAllAdmins);
-router.get("/me", adminController.getMyProfile);
+router.get('/all', adminController.getAllAdmins);
+router.get('/all/non-admins', adminController.getAllNonAdmins);
+router.get('/me', adminController.getMyProfile);
 
 router.get(
-  "/:id",
+  '/:id',
   validator(schema.userId, ValidationSource.PARAM),
   adminController.getAdmin
 );
 router.put(
-  "/:id",
-  uploadMediaFilesToThisFolder("admins"),
-  fileUploadHandler.handleSingleFileUpload("profilePicUrl"),
+  '/:id',
+  uploadMediaFilesToThisFolder('admins'),
+  fileUploadHandler.handleSingleFileUpload('profilePicUrl'),
   validator(mediaFodlerNameSchemaAndObjectId, ValidationSource.PARAM),
   validator(schema.update),
   adminController.updateAdmin
 );
 router.post(
-  "/",
-  uploadMediaFilesToThisFolder("admins"),
-  fileUploadHandler.handleSingleFileUpload("profilePicUrl"),
+  '/',
+  uploadMediaFilesToThisFolder('admins'),
+  fileUploadHandler.handleSingleFileUpload('profilePicUrl'),
   validator(schema.create),
   adminController.createAdmin
 );
 
 router.delete(
-  "/:id",
+  '/:id',
   authorization([RoleCode.SUPERADMIN]),
   validator(schema.userId, ValidationSource.PARAM),
   adminController.deleteAdmin
